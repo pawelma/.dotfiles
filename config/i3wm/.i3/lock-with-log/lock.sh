1a6240561e08ba12faf546f2aa1f35de1dfc0d21
Paweł Madejski <pmadejski@cubiware.com>
2016-08-17 17:08:25 +0200

    Update i3 config.

--- config/i3wm/.i3/lock-with-log/lock.sh
+++ config/i3wm/.i3/lock-with-log/lock.sh
@@ -10,7 +10,15 @@ function log {
 }
 
 log "Locked"
+## pause player
+player_state=`nuvolaplayer3ctl track-info | grep State | awk '{print $2}'`
+nuvolaplayer3ctl action pause
 
+# lock
 i3lock -c 000000 --nofork
 
+# unpause player
+if [ "playing" = "$player_state" ]; then
+  nuvolaplayer3ctl action play
+fi
 log "Unlocked"
